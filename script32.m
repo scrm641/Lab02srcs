46035082fe8a445f471eb7883f18df195daf1b2b
scrm641 <84881907+scrm641@users.noreply.github.com>
2021-11-09 21:42:39 -0500

    Update script32.m

--- script32.m
+++ script32.m
@@ -1,6 +1,6 @@
 function script32()
 clc,clear
-R=dlmread('datos3.txt');
+R=dlmread('datos3.txt'); # Choose the correct file to plot results (datos3.txt or datos4.txt generated for scripts which save information in text files)
 figure(1)
 hold on
 plot(R(:,1),R(:,2))
